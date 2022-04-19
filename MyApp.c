e6797131c33f31d80bd7714fb9df4617f608cf85
Karina Nosenko <karina4nosenko@gmail.com>
2022-04-19 14:42:53 +0300

    Implemented the print user data function

--- MyApp.c
+++ MyApp.c
@@ -16,7 +16,6 @@ bool isUserAuthenticated(char username[20], char password[20]) {
     strcpy(username_buffer,token);
     token = strtok(NULL, delimiter);
     strcpy(password_buffer,token);
-    //password_buffer[strlen(password_buffer)-2] = '\0';
 
     if((strcmp(username, username_buffer) == 0) && (strcmp(password, password_buffer) == 0)) {
       fclose(stream);
@@ -29,7 +28,30 @@ bool isUserAuthenticated(char username[20], char password[20]) {
 }
 
 void printUserData(char username[20]) {
-  printf("Authenticated!\n");
+  char username_buffer[20], cardNumber[20], expirationDate[10], cvc[10];
+  char line[50];
+  char* delimiter = ",\n\r";
+  char* token;
+
+  FILE *stream = fopen("Data/UserCreditCardData.csv", "r");
+
+  while (fgets(line, 50, stream)) {
+    token = strtok(line, delimiter);
+
+    strcpy(username_buffer,token);
+    token = strtok(NULL, delimiter);
+
+    if((strcmp(username, username_buffer) == 0)) {
+      strcpy(cardNumber,token);
+      token = strtok(NULL, delimiter);
+      strcpy(expirationDate,token);
+      token = strtok(NULL, delimiter);
+      strcpy(cvc,token);
+
+      printf("____________________________\n\nUsername: %s\nCard Number: %s\nExpiration date: %s\nCVC: %s\n____________________________\n", (char*)username, (char*)cardNumber, (char*)expirationDate, (char*)cvc);
+      return;
+    }
+  }
 }
 
 int main() {
